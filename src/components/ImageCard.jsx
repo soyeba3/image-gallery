1af5d79c297874563276ab92b608e283598af71c
soyeba3 <soyebchandani@gmail.com>
2023-11-05 12:38:46 +0600

    Implemented drag and drop feature

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,16 +1,26 @@
 import { useState } from "react";
 
-function ImageCard({ item }) {
+function ImageCard({
+  item,
+  onDragStart = () => {},
+  onDragEnter = () => {},
+  onDragEnd = () => {},
+}) {
   const [visible, setVisible] = useState(false);
 
   return (
     <div
-      className="relative border border-gray-300 rounded-md hover:bg-gray-300 hover:cursor-pointer first:row-span-2 first:col-span-2"
+      draggable
+      onDragStart={onDragStart}
+      onDragEnter={onDragEnter}
+      onDragEnd={onDragEnd}
+      onDragOver={(e) => e.preventDefault()}
       onMouseOver={() => setVisible(true)}
       onMouseOut={() => setVisible(false)}
+      className={`relative border border-gray-300 rounded-md hover:bg-gray-300 first:row-span-2 first:col-span-2`}
     >
       <img className="rounded-md" src={item.img} alt="img" />
-      {visible && <input className={`absolute top-4 left-4`} type="checkbox" />}
+      {visible && <input className="absolute top-4 left-4" type="checkbox" />}
     </div>
   );
 }
